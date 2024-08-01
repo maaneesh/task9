2c2abb04200213225432f8f1175e08983d1bd66e
Manish Chaudhari <chaudharimanish07@gmail.com>
2024-08-01 14:22:49 -0400

    initialized TailwindCSS and added styling

--- basic-todo-app/src/components/header.jsx
+++ basic-todo-app/src/components/header.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function Header() {
   return (
-    <>
+    <div className="text-3xl font-bold underline">
       <div>Home</div>
       <div>History</div>
-    </>
+    </div>
   );
 }
 
--- basic-todo-app/src/components/task.jsx
+++ basic-todo-app/src/components/task.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Task() {
   let count = 0;
   return (
-    <>
+    <div className="text-3xl font-bold underline">
       <h4>Title{count + 1}</h4>
       <p>this is my task</p>
-    </>
+    </div>
   );
 }
 
